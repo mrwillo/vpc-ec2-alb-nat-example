ec3aef17b9bb8135aebad4b0c439f22528ea58c0
duy.tran <tranduy86los@gmail.com>
2021-07-19 18:27:45 +0700

    rm private subnet, nat gateway

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "company"
+    organization = "example-org-5ed821"
 
     workspaces {
       name = "vpc-ec2-alb-nat-example"
