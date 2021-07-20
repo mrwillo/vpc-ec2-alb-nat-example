f0cce0454f155c7feaeaf93eacc22783eb544967
duy.tran <tranduy86los@gmail.com>
2021-07-20 10:37:37 +0700

    init vms

--- main.tf
+++ main.tf
@@ -22,3 +22,73 @@ module "vpc" {
 #     count = 3
 #     vpc = true
 # }
+
+data "aws_vpc" "selected" {
+    id = "${module.vpc.vpc_id}"
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.selected.id}"
+ 
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+}
+
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "example"
+  description = "Security group for example usage with EC2 instance"
+  vpc_id      = "${data.aws_vpc.selected.id}"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+}
+
+# resource "aws_eip" "this" {
+#   vpc      = true
+#   instance = "${module.ec2.id[0]}"
+# }
+
+module "ec2_cluster" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  for_each = data.aws_subnet_ids.all.ids
+  name                        = "example + ${each.value}"
+  ami                         = "${data.aws_ami.amazon_linux.id}"
+  instance_type               = "t2.micro"
+  subnet_id                   = each.value
+  vpc_security_group_ids      = ["${module.security_group.security_group_id}"]
+  associate_public_ip_address = true
+}
+
+# module "ec2_cluster" {
+#   source = "terraform-aws-modules/ec2-instance/aws"
+
+#   name           = "my-cluster"
+#   instance_count = 5
+  
+#   ami                    = "ami-ebd02392"
+#   instance_type          = "t2.micro"
+#   key_name               = "user1"
+#   monitoring             = true
+#   vpc_security_group_ids = ["sg-12345678"]
+#   subnet_id              = "subnet-eddcdzz4"
+
+#   tags = {
+#     Terraform = "true"
+#     Environment = "dev"
+#   }
+# }
